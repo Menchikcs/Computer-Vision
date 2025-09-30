80ef2eb710a2abf828ed7f103ebad84562abd776
Menchikcs <slavko.popenko@gmail.com>
2025-09-30 11:51:07 +0300

    Add files via upload

---
+++ HW_lesson3.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('images/photo_me.jpg')
+print(img.shape)
+cv.rectangle(img, (330, 160), (450, 300), (0, 0, 255), 5)
+cv.putText(img, 'Popenko Vyacheslav', (300,130), cv.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255), 1)
+
+
+
+cv.imshow('image',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
