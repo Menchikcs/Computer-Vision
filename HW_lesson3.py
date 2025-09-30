0119e667143281c79cc52144336890ee16a9efc7
Menchikcs <slavko.popenko@gmail.com>
2025-09-30 11:52:06 +0300

    Update HW_lesson3.py

--- HW_lesson3.py
+++ HW_lesson3.py
@@ -3,11 +3,12 @@ import numpy as np
 
 img = cv.imread('images/photo_me.jpg')
 print(img.shape)
-cv.rectangle(img, (330, 160), (450, 300), (0, 0, 255), 5)
-cv.putText(img, 'Popenko Vyacheslav', (300,130), cv.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255), 1)
+cv.rectangle(img, (330, 160), (450, 300), (255, 255, 255), 5)
+cv.putText(img, 'Popenko Vyacheslav', (300,130), cv.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), 1)
 
 
 
 cv.imshow('image',img)
 cv.waitKey(0)
+
 cv.destroyAllWindows()
